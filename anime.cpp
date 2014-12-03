aee4685785a028ecabce203d84c7796980a648be
A Random Coder <admin@shinjiru.me>
2014-12-03 01:33:42 -0500

    Implement more fields to Anime object

--- anime.cpp
+++ anime.cpp
@@ -1,5 +1,27 @@
 #include "anime.h"
 
-Anime::Anime()
-{
-}
+Anime::Anime() {}
+
+QString     Anime::getID()                                { return id; }
+void        Anime::setID(QString id)                      { this ->id = id; }
+
+QStringList Anime::getSynonyms()                          { return synonyms; }
+void        Anime::addSynonym(QString synonym)            { synonyms.append(synonym); }
+
+QString     Anime::getRomajiTitle()                       { return romaji_title; }
+void        Anime::setRomajiTitle(QString title)          { this ->romaji_title = title; }
+
+QString     Anime::getJapaneseTitle()                     { return japanese_title; }
+void        Anime::setJapaneseTitle(QString title)        { this ->japanese_title = title; }
+
+QString     Anime::getEnglishTitle()                      { return english_title; }
+void        Anime::setEnglishTitle(QString title)         { this ->english_title = title; }
+
+QString     Anime::getType()                              { return type; }
+void        Anime::setType(QString type)                  { this ->type = type; }
+
+QString     Anime::getAiringStatus()                      { return airing_status; }
+void        Anime::setAiringStatus(QString airing_status) {this ->airing_status = airing_status; }
+
+int         Anime::getEpisodeCount()                      { return episode_count; }
+void        Anime::setEpisodeCount(int episode_count)     { this ->episode_count = episode_count; }
--- anime.h
+++ anime.h
@@ -4,18 +4,43 @@
 #include <QString>
 #include <QStringList>
 
-class Anime
-{
+class Anime {
 public:
   Anime();
-  QString getID() { return id; }
-  void addSynonym(QString syn) { synonyms.append(syn); }
-  QStringList getSynonyms() { return synonyms; }
-  void setID(QString id) { this->id = id; }
+
+  QString getID();
+  void setID(QString id);
+
+  QStringList getSynonyms();
+  void addSynonym(QString synonym);
+
+  QString getRomajiTitle();
+  void setRomajiTitle(QString title);
+
+  QString getJapaneseTitle();
+  void setJapaneseTitle(QString title);
+
+  QString getEnglishTitle();
+  void setEnglishTitle(QString title);
+
+  QString getType();
+  void setType(QString type);
+
+  QString getAiringStatus();
+  void setAiringStatus(QString airing_status);
+
+  int getEpisodeCount();
+  void setEpisodeCount(int episode_count);
 
 private:
   QString id;
   QStringList synonyms;
+  QString romaji_title;
+  QString japanese_title;
+  QString english_title;
+  QString type;
+  QString airing_status;
+  int episode_count;
 };
 
 #endif // ANIME_H
