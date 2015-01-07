6a38859c008014d9432bc0dd05b4db2b59c245be
A Random Coder <matt.wilmink@gmail.com>
2015-01-06 21:07:51 -0500

    asdf

--- src/app.h
+++ src/app.h
@@ -7,7 +7,7 @@ const QString appName    = "Shinjiru";
 const QString appVersion = "0.0.1";
 const QString appAuthor  = "Kazakuri";
 
-const QString api_id     = "***REMOVED***";
-const QString api_secret = "***REMOVED***";
+const QString api_id     = "";
+const QString api_secret = "";
 
 #endif // APP_H
