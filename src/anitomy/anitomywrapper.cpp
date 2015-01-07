13da97bcc28ba125ce775705b8748840fd6077f8
A Random Coder <matt.wilmink@gmail.com>
2015-01-06 22:04:19 -0500

    Standardize the resolution detection

--- src/anitomy/anitomywrapper.cpp
+++ src/anitomy/anitomywrapper.cpp
@@ -30,7 +30,18 @@ QMap<QString, QString> AnitomyWrapper::parse(QString file_name) {
   data.insert("title",   QString::fromWCharArray(elements.get(anitomy::kElementAnimeTitle).c_str()));
   data.insert("episode", QString::fromWCharArray(elements.get(anitomy::kElementEpisodeNumber).c_str()));
   data.insert("subs",    QString::fromWCharArray(elements.get(anitomy::kElementReleaseGroup).c_str()));
-  data.insert("res",     QString::fromWCharArray(elements.get(anitomy::kElementVideoResolution).c_str()));
+
+  QString res = QString::fromWCharArray(elements.get(anitomy::kElementVideoResolution).c_str());
+
+  // Standardize the resolutions a bit instead of having 720p or 1280x720 available
+  if(res.contains("x")) {
+      res = res.split("x").last() + "p";
+  } else if(res.contains("X")) {
+      res = res.split("X").last() + "p";
+  } else if(res.isEmpty()) {
+      res = "720p"; // If we don't have a resolution, it's probably 720p so let's assume it is.
+  }
+  data.insert("res", res);
 
   return data;
 }
