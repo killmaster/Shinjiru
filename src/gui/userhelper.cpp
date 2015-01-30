d20b525055e05f9ecf037e9f74ae55a5119ae032
A Random Coder <matt.wilmink@gmail.com>
2015-01-30 12:48:43 -0500

    Draw current notification count

--- src/gui/userhelper.cpp
+++ src/gui/userhelper.cpp
@@ -22,6 +22,21 @@ void MainWindow::userLoaded() {
   progress_bar->setValue(20);
   progress_bar->setFormat("User Loaded");
 
+  QPixmap *pix = new QPixmap(width(), height());
+  pix->fill(Qt::transparent);
+
+  QPainter p(pix);
+  p.setPen(QColor(30,30,30));
+  p.setBrush(QColor(255,255,255));
+
+  int notification_count = User::sharedUser()->notificationCount();
+  if(notification_count > 0) p.setBrush(QColor(255,120,120));
+  p.drawEllipse(width() - 25.0f,60.0f, 20.0f, 20.0f);
+  QString notif = notification_count >= 9 ? "9+" : QString::number(notification_count);
+  p.drawText(width() - 25.0f, 59.0f, 20.0f, 20.0f, Qt::AlignCenter, notif);
+
+  over->addDrawing("notifications", pix);
+
   connect(User::sharedUser(), SIGNAL(new_image()), SLOT(repaint()));
   repaint();
 
